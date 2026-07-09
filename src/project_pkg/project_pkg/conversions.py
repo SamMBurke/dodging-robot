cb4a2effdf0e063a972a4b2f8d1fb880767c89eb
SamMBurke <s8burke@uwaterloo.ca>
2026-07-09 17:27:37 -0400

    function description format changes

--- src/project_pkg/project_pkg/conversions.py
+++ src/project_pkg/project_pkg/conversions.py
@@ -3,9 +3,13 @@ from geometry_msgs.msg import Point
 
 
 def corners_to_ros(corners):
-    '''Converts an (N, 2) numpy array of corner points into a list of geometry_msgs/Point'''
+    '''
+    Converts an (N, 2) numpy array of corner points into a list of geometry_msgs/Point
+    '''
     return [Point(x=float(c[0]), y=float(c[1]), z=0.0) for c in corners]
 
 def corners_from_ros(corners_msg):
-    '''Converts a list of geometry_msgs/Point into an (N, 2) numpy array'''
+    '''
+    Converts a list of geometry_msgs/Point into an (N, 2) numpy array
+    '''
     return np.array([[p.x, p.y] for p in corners_msg])
